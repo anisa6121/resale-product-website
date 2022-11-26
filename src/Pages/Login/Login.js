dc0deaa2024575825edcea773acb13d96a1faf88
anisa6121 <you@example.com>
2022-11-26 11:31:47 +0600

    login-add

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,3 +1,157 @@
+import React, { useContext, useState } from "react";
+import { useForm } from "react-hook-form";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthProvider";
 
+const Login = () => {
+	const { signIn } = useContext(AuthContext);
+
+	const [loginError, setLoginError] = useState("");
+
+	const [loginUserEmail, setLoginUserEmail] = useState("");
+	//  const [token] = useToken(loginUserEmail);
+
+	const location = useLocation();
+	const navigate = useNavigate();
+
+	const from = location.state?.from?.pathname || "/";
+
+	//  if (token) {
+	// 		navigate(from, { replace: true });
+	//  }
+	const {
+		register,
+		formState: { errors },
+		handleSubmit,
+	} = useForm();
+
+	const handleLogin = (data) => {
+		console.log(data);
+
+		setLoginError("");
+
+		signIn(data.email, data.password)
+			.then((result) => {
+				const user = result.user;
+				console.log(user);
+
+				setLoginUserEmail(data.email);
+			})
+
+			.catch((error) => {
+				console.log(error.message);
+				setLoginError(error.message);
+			});
+	};
+
+	return (
+		<div className="h-[800px] flex justify-center items-center">
+			<div className="w-96 p-7">
+				<h2 className="text-3xl text-center"> LogIn </h2>
+
+				<form onSubmit={handleSubmit(handleLogin)}>
+					{/* daisy ui */}
+
+					<div className="form-control w-full max-w-xs">
+						<label className="label">
+							<span className="label-text">
+								Email
+							</span>
+						</label>
+
+						<input
+							type="text"
+							{...register("email", {
+								required: "Email Address is required",
+							})}
+							className="input input-bordered w-full max-w-xs"
+						/>
+						{errors.email && (
+							<p
+								className="text-red-600"
+								role="alert"
+							>
+								{errors.email?.message}
+							</p>
+						)}
+					</div>
+
+					<div className="form-control w-full max-w-xs">
+						<label className="label">
+							<span className="label-text">
+								Password
+							</span>
+						</label>
+
+						<input
+							type="password"
+							{...register("password", {
+								required: "Password is required",
+
+								minLength: {
+									value: 6,
+									message: "Password must be 6 characters or longer",
+								},
+							})}
+							className="input input-bordered w-full max-w-xs"
+						/>
+						{errors.password && (
+							<p
+								className="text-red-600"
+								role="alert"
+							>
+								{
+									errors.password
+										?.message
+								}
+							</p>
+						)}
+
+						<label className="label">
+							{/* <span className="label-text">
+								Forget Password?
+							</span> */}
+
+							<button
+								// onClick={handleReset}
+								className="text-xs hover:underline"
+							>
+								Forget Password?
+							</button>
+						</label>
+					</div>
+
+					<input
+						className="btn btn-primary w-full"
+						value="Login"
+						type="submit"
+					/>
+
+					<div>
+						{loginError && (
+							<p className="text-red-600">
+								{loginError}
+							</p>
+						)}
+					</div>
+				</form>
+
+				<p>
+					New to This Website
+					<Link
+						className="text-secondary"
+						to="/register"
+					>
+						Create new Account
+					</Link>
+				</p>
+				<div className="divider">OR</div>
+				<button className="btn btn-error btn-outline w-full">
+					Log In WITH GOOGLE
+				</button>
+			</div>
+		</div>
+	);
+};
 
 export default Login;
